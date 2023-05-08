a8dafe9b2506ff5b6543153f6a5a41474c5a5926
Aleksey <mc_gra_dy@mail.ru>
2023-05-08 14:22:54 +0300

    исправил повидение video

--- src/video.py
+++ src/video.py
@@ -1,10 +1,26 @@
+import os
+from googleapiclient.discovery import build
 class Video:
+
     def __init__(self, video_id):
         self.video_id = video_id
+        api_key = os.getenv('API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        video = youtube.videos().list(part='snippet,statistics', id=video_id).execute()['items'][0]['snippet']
 
+        self.title = video['title']
+        self.url = f"https://www.youtube.com/watch?v={video_id}"
+       # self.views = video['statistics']['viewCount']
+       # self.likes = video['statistics']['likeCount']
+
+    def __str__(self):
+        return f"{self.title}"
 
 
 class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
+
+    def __str__(self):
+        return f"{super().__str__()}"
