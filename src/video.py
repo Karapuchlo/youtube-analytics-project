6a5c65417509abfe9c92e049c39a841de7a9c113
Aleksey <mc_gra_dy@mail.ru>
2023-05-19 18:26:44 +0300

    вывел исключения

--- src/video.py
+++ src/video.py
@@ -7,12 +7,19 @@ class Video:
         api_key = os.getenv('API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
         video = youtube.videos().list(part='snippet,statistics,contentDetails', id=video_id).execute()['items'][0]['snippet']
-        self.duration = duration
+        try:
+            self.duration = duration
 
-        self.title = video['title']
-        self.url = f"https://www.youtube.com/watch?v={video_id}"
+            self.title = video['title']
+            self.url = f"https://www.youtube.com/watch?v={video_id}"
        # self.views = video['statistics']['viewCount']
        # self.likes = video['statistics']['likeCount']
+        except requests.exceptions.RequestException:
+            print(f"Failed to fetch data for video id {video_id}.")
+        except KeyError:
+            print(f"Data for video id {video_id} is incomplete.")
+        except Exception as e:
+            print(f"Unexpected error while fetching data for video id {video_id}: {e}.")
 
     def __str__(self):
         return f"{self.title}"
